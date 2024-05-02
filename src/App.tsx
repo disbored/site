001e1adf032f1d7ef984cfca8639a09d4aa75981
Naibuu <hawkwer141099@gmail.com>
2024-05-02 21:59:37 +0300

    add: base project

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+    return (
+        <div className="w-full h-screen flex justify-center items-center">
+            <h4 className="text-neutral-200">disbored.</h4>
+        </div>
+    )
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ['./index.html', './src/**/*.{ts,tsx}'],
+    theme: {
+        fontFamily: {
+            mono: ['JetBrains Mono', 'sans-serif'],
+        },
+        extend: {},
+    },
+    plugins: [],
+}
