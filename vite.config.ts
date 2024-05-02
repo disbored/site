8a5092621e95641f6f61c5eff735c0179ec7dbfd
Naibuu <hawkwer141099@gmail.com>
2024-05-02 22:06:57 +0300

    chore: switch to vercel instead of gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,5 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 /** https://vitejs.dev/config/ */
 export default defineConfig({
-    base: 'https://disbored.github.io/site',
     plugins: [react(), tsconfigPaths()],
 })
