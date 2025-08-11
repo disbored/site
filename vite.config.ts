7e696bda80f91de176e65b952227a0db09db4eb5
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2025-08-11 17:32:06 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 /** https://vitejs.dev/config/ */
 export default defineConfig({
-    base: 'https://disbored.github.io/site',
+    base: 'https://surg.fyi',
     plugins: [react(), tsconfigPaths()],
 })
