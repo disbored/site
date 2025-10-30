b85743f6f1ce55c46ea166c2621ea4b3e8971404
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2025-10-30 12:13:17 -0400

    package??

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,13 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 /** https://vitejs.dev/config/ */
 export default defineConfig({
-    //    base: 'https://surg.fyi',
     plugins: [react(), tsconfigPaths()],
+    server: {
+        port: 8080,
+        strictPort: true,
+    },
+    preview: {
+        port: 8080,
+        strictPort: true,
+    },
 })
